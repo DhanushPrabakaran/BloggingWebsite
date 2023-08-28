e51843b1ebeeb2afff60e076d15bab134718167f
Dhanush Prabakaran <105353897+DhanushPrabakaran@users.noreply.github.com>
2023-08-28 16:18:37 +0530

    changes in Header and body content

---
+++ app/Portfolio/page.tsx
@@ -0,0 +1,14 @@
+
+
+import Header from "@/components/Header"
+export default function page() {
+  return (
+<>
+<Header/>
+<h1>Portfolio</h1>
+</>
+    
+
+    
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
 
+import Background from "@/components/Background"
 export default function Home() {
   return (
-    <h1>
-      helllo
-    </h1>
+
+    <Background/>
+
+    
   )
 }
