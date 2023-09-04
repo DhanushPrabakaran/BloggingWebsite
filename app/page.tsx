fe0091861c0736902d92ea9eb60d45d6bbf120bb
Dhanush Prabakaran <105353897+DhanushPrabakaran@users.noreply.github.com>
2023-09-04 08:33:36 +0530

    changes in home and tech nology layout

--- app/page.tsx
+++ app/page.tsx
@@ -21,31 +21,31 @@ export default function page() {
 background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cg fill='%23015f77' fill-opacity='1'%3E%3Cpolygon fill-rule='evenodd' points='8 4 12 6 8 8 6 12 4 8 0 6 4 4 6 0 8 4'/%3E%3C/g%3E%3C/svg%3E"); */}
        <div
        className="w-full">
-       <div className="w-full p-2 flex justify-center items-center">
+       <div className="w-full px-2  flex justify-center text-white items-center">
        <Link
-            href={"/Contact"}
+            href={"/"}
+            className=" font-bold font-sans bg-slate-900  italic  w-full mb-1 rounded-md text-center  flex-grow border-2 border-slate-900 "
+          >
+            #Home
+          </Link>
+       </div>
+       <div className="w-full px-2 flex justify-center items-center">
+       <Link
+            href={"/Technologys"}
             className=" font-bold font-sans italic  w-full mb-1 rounded-md text-center  flex-grow border-2 border-slate-900 "
           >
-            #Home
+            #Technologys
           </Link>
        </div>
-       <div className="w-full p-2 flex justify-center items-center">
+       <div className="w-full px-2 flex justify-center items-center">
        <Link
             href={"/Contact"}
-            className=" font-bold font-sans italic  w-full mb-1 rounded-md text-center  flex-grow border-2 border-slate-900 "
+            className=" font-bold font-sans italic  w-full mb-1  rounded-md text-center  flex-grow border-2 border-slate-900 "
           >
             #Home
           </Link>
        </div>
-       <div className="w-full p-2 flex justify-center items-center">
-       <Link
-            href={"/Contact"}
-            className=" font-bold font-sans italic  w-full  rounded-md text-center  flex-grow border-2 border-slate-900 "
-          >
-            #Home
-          </Link>
-       </div>
-       <div className="w-full p-2 flex justify-center items-center">
+       <div className="w-full px-2 flex justify-center items-center">
        <Link
             href={"/Contact"}
             className=" font-bold font-sans italic  w-full mb-1 rounded-md text-center  flex-grow border-2 border-slate-900 "
